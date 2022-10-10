265e4b31bbca5b4611ffe484a7385a5e23d4dc52
AlexMoyAlex <110425356+AlexMoyAlex@users.noreply.github.com>
2022-10-10 10:13:54 +0300

    git commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,7 @@
 package jm.task.core.jdbc;
 
-
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
 import javax.swing.text.TableView;
 import java.sql.Connection;
 import java.sql.DriverManager;
